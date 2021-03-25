588038fba2a2359835e1c9bef4bb1b3179592ac8
dhayananth <dhayanthdharma@gmail.com>
2021-03-25 14:57:37 +0100

    config update

--- user-password.py
+++ user-password.py
@@ -1,5 +1,5 @@
 # (u'DG Regio', BotPassword(u'DG_Regio_/_Kohesio_Bot', u'935fsj7p9ro101dc0uevt3q1mtf6it0t'))
 
 # SERVER
-(u'WikibaseAdmin', BotPassword(u'bot_user_1', u'c98shh4r4mtaj1gevu1i7h71n45aortn'))
+(u'WikibaseAdmin', BotPassword(u'bot_user_1', u'marcronujfavlgp4l8tmu75l7gvqsap5'))
 # (u'WikibaseAdmin', BotPassword(u'bot_user_1', u'da8vpt7lggpkp6uj47kcnb6brv2l4p3a'))
